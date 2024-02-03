5c909f6c78c988a1931a9d1f3245ad889f258d84
Przemek Kosakowski (przemosk) <p.kosakowski@gmail.com>
2024-02-03 16:56:46 +0100

    Change main DOM element

--- main.go
+++ main.go
@@ -16,14 +16,15 @@ type Book struct {
 }
 
 func main() {
-	c := colly.NewCollector()
 	var books []Book
 
+	c := colly.NewCollector()
+
 	c.OnRequest(func(r *colly.Request) {
-		// fmt.Println("Visiting", r.URL)
+		log.Print("VISITING ", r.URL)
 	})
 
-	c.OnHTML("#listTop100", func(e *colly.HTMLElement) {
+	c.OnHTML(".container", func(e *colly.HTMLElement) {
 		e.ForEach(".authorAllBooks__single", func(_ int, e *colly.HTMLElement) {
 			image_url := e.ChildAttr("img", "src")
 			author_name := e.ChildText(".authorAllBooks__singleTextAuthor")
@@ -35,24 +36,11 @@ func main() {
 			books = append(books, book)
 			log.Print(book)
 		})
-	})
 
-	c.OnHTML("#listTop100Paginator", func(e *colly.HTMLElement) {
-		e.ForEach(".authorAllBooks__single", func(_ int, e *colly.HTMLElement) {
-			image_url := e.ChildAttr("img", "src")
-			author_name := e.ChildText(".authorAllBooks__singleTextAuthor")
-			book_title := e.ChildText(".authorAllBooks__singleTextTitle")
-			author_url := e.ChildAttr(".authorAllBooks__singleTextAuthor a", "href")
-			rating := e.ChildText(".listLibrary__ratingAll")
-
-			book := Book{Title: book_title, Author: author_name, AuthorURL: author_url, ImageURL: image_url, Votes: rating}
-			books = append(books, book)
-			log.Print(book)
-		})
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Error", err)
+		log.Fatal(err)
 	})
 
 	var total_page int = 7
